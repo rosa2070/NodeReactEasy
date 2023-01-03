f8ffab38d717d23bc2360d72bee9ea5848e5c12f
jaeeun <rosa2070@naver.com>
2023-01-03 21:59:54 +0900

    register 라우터 생성

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-require('dotenv').config(); // .env 파일에서 환경변수 불러오기
-
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 타입의 데이터를 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 타입의 데이터를 분석해서 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI, {})
+mongoose.connect('mongodb+srv://jaeeun:981224@boilerplate.nbsovwf.mongodb.net/?retryWrites=true&w=majority', {})
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!새해 복 많이')
+})
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
