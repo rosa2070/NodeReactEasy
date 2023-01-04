e4754c989b0d6be766a170a342e4117fca7fd137
jaeeun <rosa2070@naver.com>
2023-01-04 19:47:31 +0900

    bcrypt로 비밀번호 암호화

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const bodyParser = require('body-parser');
-
-const config = require('./config/key');
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
 
 const { User } = require("./models/User");
-
 
 //application/x-www-form-urlencoded 타입의 데이터를 분석해서 가져옴
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,31 +12,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //application/json 타입의 데이터를 분석해서 가져옴
 app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false);
-mongoose.connect(config.mongoURI, {})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.mongoURI, {})
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!새해 복 많이많이')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!새해 복 많이많이");
+});
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
+  //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
 
-    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
-    //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body);
 
-    const user = new User(req.body)
-
-    user.save((err, userInfo) => {
-        if (err) return res.json({ success: false, err })
-        return res.status(200).json({
-            success: true
-        })
-    })
-})
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- models/User.js
+++ models/User.js
@@ -1,39 +1,66 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true, //이메일 안 스페이스바 없애주는 역할
-        unique: 1
-    },
-    password: {
-        type: String,
-        minlength: 5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    image: String,
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, //이메일 안 스페이스바 없애주는 역할
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 
-//Schema를 model로 감싸준다.
-//매개변수 User는 모델이름, userSchema는 Schema를 의미
-const User = mongoose.model('User', userSchema)
+//usermodel에 정보를 저장하기 전에 무언가를 한다.
+userSchema.pre("save", function (next) {
+  //this는 User.js의 schema부분을 가리킴
+  var user = this;
+
+  //password가 변환된 때만 암호화 해줌
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      //에러가 나면 바로 user.save로 이동
+      if (err) return next(err);
+
+      //user.password는 암호화 전 비밀번호, hash는 암호화된 비밀번호
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        //에러 나면 user.save로 돌려보냄
+        if (err) return next(err);
+        //암호화된 비밀번호 만드는데 성공하면 비밀번호를 hash로 교체
+        user.password = hash;
+        //next를 통해 index.js에 있는 user.save로 이동
+        next();
+      });
+    });
+  }
+});
+
+//Schema를 model로 감싸준다. 매개변수 User는 모델이름, userSchema는 Schema를 의미
+const User = mongoose.model("User", userSchema);
 
 //모델을 다른 곳에서도 쓸수있게 export
-module.exports = { User }
+module.exports = {
+  User,
+};
