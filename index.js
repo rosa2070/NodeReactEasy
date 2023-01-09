0873eafcc968d74d7f55bf0581df2fb5f7861531
jaeeun <rosa2070@naver.com>
2023-01-09 14:15:32 +0900

    로그인 라우터 생성

--- index.js
+++ index.js
@@ -37,6 +37,42 @@ app.post("/register", (req, res) => {
   });
 });
 
+app.post('/login', (req, res) => {
+  
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    // 요청된 이메일을 가진 user가 한명도 없는 경우
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일을 가진 user가 있는 경우
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+    //isMatch메서드는 User.js에 만든다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      //비밀번호가 같지 않다면
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+      
+        //비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        
+      })
+
+    })
+
+
+
+  
+  })
+
+
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- models/User.js
+++ models/User.js
@@ -54,8 +54,26 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next()
   }
-});
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  
+  //plainPassword 1234567         암호화된 비밀번호 $2b$10$lKPmVfw8.rX1nInu8c10W.FqYH3tu6xMbUXdcdkh9QSkJ0VjSvxA2
+  //plainPassword를 암호화한 후 db에 있는 비밀번호와 같은지 확인하여야 한다.
+  //this는 userSchema를 의미
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    // 비밀번호가 같지 않다.
+    if (err) return cb(err),
+    // 비밀번호가 일치한다.
+    // null은 에러가 없음을 의미
+    // isMatch 값은 true이다.
+    cb(null, isMatch)
+
+  })
+}
 
 //Schema를 model로 감싸준다. 매개변수 User는 모델이름, userSchema는 Schema를 의미
 const User = mongoose.model("User", userSchema);
